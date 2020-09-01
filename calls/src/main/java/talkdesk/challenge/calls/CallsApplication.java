c499bbb4272c3c6184334d6f9fe671ab761f0319
Ana Cruz <anapatriciacruz@ua.pt>
2020-09-01 18:19:46 +0100

    init

---
+++ calls/src/main/java/talkdesk/challenge/calls/CallsApplication.java
@@ -0,0 +1,13 @@
+package talkdesk.challenge.calls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallsApplication.class, args);
+    }
+
+}
---
+++ calls/src/test/java/talkdesk/challenge/calls/CallsApplicationTests.java
@@ -0,0 +1,13 @@
+package talkdesk.challenge.calls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
